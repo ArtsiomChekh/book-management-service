0b34eff4e66a704dd55222e37f22869dd3f60e5d
ArtsiomChekh <qbartem@gmail.com>
2023-02-13 11:09:30 +0300

    End Commit

--- book-management-service-app/src/main/java/com/chekh/artsiom/repository/BookUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chekh.artsiom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.chekh.artsiom.model.BookUser;
-
-public interface BookUserRepository extends JpaRepository<BookUser, Long>{
-
-}
